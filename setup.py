c32ed06a7304b357a2fdd18fd81ed7e586bdc5e5
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-09-11 19:05:07 -0500

    docs(readthedocs): install nodejs)

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from subprocess import check_call
 
 class PostInstallCommand(install):
   def run(self):
+    check_call("apt-get install -y nodejs", shell=True)
     check_call("npm install -g typedoc", shell=True)
     install.run(self)
 
